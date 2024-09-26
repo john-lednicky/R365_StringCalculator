e36283b6f1566b2aa9ddeacf9d1b90da722481c7
John Lednicky <john.d.lednicky@gmail.com>
2024-09-26 18:12:10 -0500

    feat: Add two numbers

--- Services/StringCalculator.cs
+++ Services/StringCalculator.cs
@@ -2,17 +2,23 @@
 {
     public class StringCalculator
     {
+        Messages messages = new Messages();
+
         public string Add(string input)
         {
             if (input == "") return "0";
 
             string[] inputArray = input.Split(',');
+
+            //Numbers outside the range of the decimal data type will be converted to zero.
             decimal[] decimalArray = inputArray.Select((s) => {
                     decimal val = 0;
                     decimal.TryParse(s, out  val);
                     return val;
                 })
                 .ToArray();
+
+            if (decimalArray.Length > 2) throw new FormatException(messages["MoreThanTwoNumbers"]);
 
             List<decimal> decimalList = new List<decimal>(decimalArray);
 
