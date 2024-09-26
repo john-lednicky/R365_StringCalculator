172c8b9e156af0aa17526478a05038350b94991e
John Lednicky <john.d.lednicky@gmail.com>
2024-09-26 18:33:33 -0500

    feat: Disallow negative numbers

--- Services/StringCalculator.cs
+++ Services/StringCalculator.cs
@@ -22,9 +22,21 @@
                 })
                 .ToArray();
 
+            AssertNoNegativeNumbers(decimalArray);
+
             List<decimal> decimalList = new List<decimal>(decimalArray);
 
             return decimalList.Sum().ToString();
         }
+
+        private void AssertNoNegativeNumbers(decimal[] decimalArray)
+        {
+            decimal[] negativeNumbers = decimalArray.Where(d => d < 0).ToArray();
+            if (negativeNumbers.Length > 0)
+            {
+                throw new FormatException($"{messages["NegativeNumbers"]} Input contained {string.Join(",", negativeNumbers)}");
+            }
+        }
     }
+
 }
