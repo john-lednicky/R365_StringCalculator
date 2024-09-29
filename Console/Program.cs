338cff85e9759ada79d62f1de392f04fdeffcf67
John Lednicky <john.d.lednicky@gmail.com>
2024-09-29 15:32:16 -0500

    refactor: use System.CommandLine

--- Console/Program.cs
+++ Console/Program.cs
@@ -2,7 +2,7 @@
 {
     using Microsoft.Extensions.DependencyInjection;
     using System;
-    using CommandLine;
+    using System.CommandLine;
     using StringCalculator.Services;
     using StringCalculator.Common;
     
@@ -17,32 +17,50 @@
                 .AddSingleton<StringCalculator>()
                 .BuildServiceProvider();
 
-            Parser.Default.ParseArguments<Options>(args)
-                   .WithParsed<Options>(o =>
-                   {
-                       var sc = services.GetRequiredService<StringCalculator>();
-                       var result = sc.Add(o.Input, o.Delimiter, o.AllowNegatives, o.NumberLimit);
-                       Console.WriteLine(result);
-                   });
+            var inputArgument = new Argument<string>(
+                name: "input",
+                description: "Numbers to process with optional prefix to define delimiters."
+                );
+
+            var delimiterOption = new Option<string>(
+                name: "--delimiter",
+                description: "Alternate delimiter to use instead of newline.",
+                getDefaultValue: () => "/n"
+                );
+            delimiterOption.AddAlias("-d");
+
+            var allowNegativesOption = new Option<bool>(
+                name: "--allowNegatives",
+                description: "Allow negatives? If not specified, an exception will be thrown when a negative number is encountered in the input.",
+                getDefaultValue: () => false
+                );
+            allowNegativesOption.AddAlias("-n");
+
+            var numberLimitOption = new Option<int>(
+                name: "--numberLimit",
+                description: "Highest number to process. Numbers in input higher than this are converted to zero.",
+                getDefaultValue: () => 1000
+                );
+            numberLimitOption.AddAlias("-l");
+
+            var rootCommand = new RootCommand();
+            rootCommand.Add(inputArgument);
+            rootCommand.Add(delimiterOption);
+            rootCommand.Add(allowNegativesOption);
+            rootCommand.Add(numberLimitOption);
+
+            rootCommand.SetHandler(async (inputArgumentValue, delimiterOptionValue, allowNegativesOptionValue, numberLimitOptionValue) => {
+
+                var sc = services.GetRequiredService<StringCalculator>();
+                var result = sc.Add(inputArgumentValue, delimiterOptionValue, allowNegativesOptionValue, numberLimitOptionValue);
+                Console.WriteLine(result);
+            },
+                inputArgument, delimiterOption, allowNegativesOption, numberLimitOption
+            );
+
+            rootCommand.InvokeAsync(args);
+
         }
     }
 
-    internal class Options
-    {
-        #pragma warning disable CS8618 // String might be null warning not needed because of attribute default
-
-        [Value(0, Required = true, MetaName = "input", HelpText = "Input string")]
-        public string Input { get; set; }
-
-        [Option('d', "delimiter", Default="\n", HelpText = "Alternate delimiter")]
-        public string Delimiter { get; set; }
-
-        #pragma warning restore CS8618 // String might be null warning not needed because of attribute default
-
-        [Option('n', "allowNegatives", Default=false, HelpText = "Allow negative numbers")]
-        public bool AllowNegatives { get; set; }
-
-        [Option('l', "numberLimit", Default = 1000, HelpText = "Maximum number limit")]
-        public int NumberLimit { get; set; }
-    }
 }
